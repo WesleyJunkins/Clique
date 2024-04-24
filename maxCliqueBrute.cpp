5fe2d9231a425f1c52e225895f9fe23651a4b045
WesleyJunkins <67446010+WesleyJunkins@users.noreply.github.com>
2024-04-24 15:56:25 -0500

    I think these are both good

--- maxCliqueBrute.cpp
+++ maxCliqueBrute.cpp
@@ -1,140 +1,74 @@
-// #include <iostream>
-// #include <fstream>
-// #include <vector>
-// #include <sstream>
-
-// using namespace std;
-
-// // Function to check if a given set of nodes forms a clique
-// bool isClique(const vector<vector<int>> &graph, int subset)
-// {
-//     int n = graph.size();
-//     for (int i = 0; i < n; i++)
-//     {
-//         if (!(subset & (1 << i)))
-//         {
-//             continue;
-//         }
-//         for (int j = i + 1; j < n; j++)
-//         {
-//             if ((subset & (1 << j)) && graph[i][j] == 0)
-//             {
-//                 return false;
-//             }
-//         }
-//     }
-//     return true;
-// }
-
-// // Function to count set bits in an integer
-// int countBits(int subset)
-// {
-//     int count = 0;
-//     while (subset)
-//     {
-//         count += subset & 1;
-//         subset >>= 1;
-//     }
-//     return count;
-// }
-
-// // Main function to find the maximum clique
-// int maximumClique(const vector<vector<int>> &graph)
-// {
-//     int n = graph.size();
-//     int maxCliqueSize = 0;
-//     int bestClique = 0;
-
-//     for (int i = 1; i < (1 << n); i++)
-//     {
-//         if (isClique(graph, i))
-//         {
-//             int subsetSize = countBits(i);
-//             if (subsetSize > maxCliqueSize)
-//             {
-//                 maxCliqueSize = subsetSize;
-//                 bestClique = i;
-//             }
-//         }
-//     }
-
-//     cout << "Maximum Clique Size: " << maxCliqueSize << endl;
-//     cout << "Nodes in the Maximum Clique: ";
-//     for (int i = 0; i < n; i++)
-//     {
-//         if (bestClique & (1 << i))
-//             cout << i << " ";
-//     }
-//     cout << endl;
-
-//     return maxCliqueSize;
-// }
-
-// int main(int argc, char *argv[])
-// {
-//     ifstream fin(argv[1]);
-//     if (!fin)
-//     {
-//         cerr << "Error opening file." << endl;
-//         return 1;
-//     }
-
-//     vector<vector<int>> graph;
-//     string line;
-//     int max_node_index = -1;
-
-//     // Read the graph
-//     while (getline(fin, line))
-//     {
-//         vector<int> row;
-//         istringstream iss(line);
-//         int node, connection;
-//         iss >> node; // Read the node number
-//         while (iss >> connection)
-//         {
-//             row.push_back(connection);
-//         }
-//         graph.push_back(row);
-//         max_node_index = max(max_node_index, node);
-//     }
-//     fin.close();
-
-//     // Ensure all rows have correct number of elements
-//     for (auto &row : graph)
-//     {
-//         if (row.size() < graph.size())
-//         {
-//             row.resize(graph.size(), 0); // Fill missing connections with 0
-//         }
-//     }
-
-//     // Debug: Print the graph
-//     cout << "Graph structure:" << endl;
-//     for (const auto &row : graph)
-//     {
-//         for (int elem : row)
-//         {
-//             cout << elem << " ";
-//         }
-//         cout << endl;
-//     }
-
-//     maximumClique(graph);
-
-//     return 0;
-// }
-
-
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <vector>
-
 using namespace std;
 
-// Function to check if a given set of nodes forms a clique
-bool isClique(const vector<vector<int>>& graph, const vector<int>& subset) {
-    for (int i = 0; i < subset.size(); i++) {
-        for (int j = i + 1; j < subset.size(); j++) {
-            if (graph[subset[i]][subset[j]] == 0) {  // Check if there is no edge between any two nodes in the subset
+class node
+{
+public:
+    vector<int> connectedNodes;
+    int nodeName;
+};
+
+vector<node *> readFile(string fileName)
+{
+    ifstream file(fileName);
+    if (!file.is_open())
+    {
+        cout << fileName << " could not be opened" << endl;
+        vector<node *> emptyVector;
+        return emptyVector;
+    };
+    vector<node *> graph;
+    string textLine;
+    int rowNumber = 0;
+    while (getline(file, textLine))
+    {
+        node *rowNode = new node();
+        rowNode->nodeName = rowNumber;
+        graph.push_back(rowNode);
+        istringstream iss(textLine);
+        int doNotUseRowNumberThisIsStupid;
+        iss >> doNotUseRowNumberThisIsStupid;
+        int columnNumber = 0;
+        int connectStatus;
+        while (iss >> connectStatus)
+        {
+            if ((connectStatus == 1) && (rowNumber != columnNumber))
+            {
+                graph.at(rowNumber)->connectedNodes.push_back(columnNumber);
+                graph.at(columnNumber)->connectedNodes.push_back(rowNumber);
+            };
+            columnNumber++;
+        }
+        rowNumber++;
+    };
+    return graph;
+};
+
+bool isConnectionBetween(node *node1, node *node2, vector<node *> graph)
+{
+    bool isConnection = false;
+    for (int i = 0; i < node1->connectedNodes.size(); i++)
+    {
+        if (node2->nodeName == node1->connectedNodes.at(i))
+        {
+            isConnection = true;
+        }
+    }
+    return isConnection;
+};
+
+bool isClique(const vector<node *> &clique)
+{
+    for (int i = 0; i < clique.size(); i++)
+    {
+        for (int j = i + 1; j < clique.size(); j++)
+        {
+            if (find(clique[i]->connectedNodes.begin(), clique[i]->connectedNodes.end(), clique[j]->nodeName) == clique[i]->connectedNodes.end())
+            {
                 return false;
             }
         }
@@ -142,57 +76,51 @@ bool isClique(const vector<vector<int>>& graph, const vector<int>& subset) {
     return true;
 }
 
-// Recursive function to generate all subsets
-void generateSubsets(vector<vector<int>>& subsets, vector<int>& current, int start, int n) {
-    subsets.push_back(current);
-    for (int i = start; i < n; i++) {
-        current.push_back(i);
-        generateSubsets(subsets, current, i + 1, n);
-        current.pop_back();
+void findCliques(vector<node *> &graph, vector<node *> &currentClique, int startIndex, vector<vector<node *>> &allCliques)
+{
+    if (isClique(currentClique))
+    {
+        allCliques.push_back(currentClique);
+    }
+    for (int i = startIndex; i < graph.size(); i++)
+    {
+        currentClique.push_back(graph[i]);
+        findCliques(graph, currentClique, i + 1, allCliques);
+        currentClique.pop_back();
     }
 }
 
-// Function to find the maximum clique
-int maximumClique(const vector<vector<int>>& graph) {
-    int n = graph.size();
-    vector<vector<int>> subsets;
-    vector<int> current;
-    generateSubsets(subsets, current, 0, n);
+vector<vector<node *>> allCliques(vector<node *> graph)
+{
+    vector<vector<node *>> allCliques;
+    vector<node *> currentClique;
+    findCliques(graph, currentClique, 0, allCliques);
+    return allCliques;
+}
 
-    int maxCliqueSize = 0;
-    vector<int> bestClique;
+int main(int argc, char *argv[])
+{
+    vector<node *> graph = readFile(argv[1]);
 
-    for (const auto& subset : subsets) {
-        if (isClique(graph, subset)) {
-            int subsetSize = subset.size();
-            if (subsetSize > maxCliqueSize) {
-                maxCliqueSize = subsetSize;
-                bestClique = subset;
-            }
+    for (int i = 0; i < graph.size(); i++)
+    {
+        cout << "Node " << graph.at(i)->nodeName << " is connected to: ";
+        for (int j = 0; j < graph.at(i)->connectedNodes.size(); j++)
+        {
+            cout << graph.at(i)->connectedNodes.at(j) << " ";
         }
+        cout << endl;
     }
 
-    cout << "Maximum Clique Size: " << maxCliqueSize << endl;
-    cout << "Nodes in the Maximum Clique: ";
-    for (int node : bestClique) {
-        cout << node << " ";
+    vector<vector<node *>> cliques = allCliques(graph);
+    cout << "Total cliques found: " << cliques.size() << endl;
+    for (int i = 0; i < cliques.size(); i++)
+    {
+        cout << "Clique " << i + 1 << ": ";
+        for (node *n : cliques[i])
+        {
+            cout << n->nodeName << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-
-    return maxCliqueSize;
-}
-
-int main() {
-    vector<vector<int>> graph = {
-        {0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0},
-        {1, 1, 0, 0, 0, 0},
-        {0, 1, 1, 0, 0, 0},
-        {1, 0, 1, 0, 0, 0},
-        {0, 1, 1, 1, 0, 0}
-    };
-
-    maximumClique(graph);
-
-    return 0;
 }
